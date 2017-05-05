573f9aef4211b6cef40cc4d54faf9b39bc4eea21
Rachel Schwartz <rsschwartz@gmail.com>
2017-05-05 11:59:32 -0400

    add example for BtG workshop

---
+++ example.R
@@ -0,0 +1,10 @@
+#install.packages('curl')
+#install.packages('ggplot2')
+#install.packages('dplyr')
+
+#load packages
+library(curl)
+library(ggplot2)
+library(dplyr)
+
+#working with gapminder data - see gapminder.org
