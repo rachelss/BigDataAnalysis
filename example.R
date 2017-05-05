41f3eeda4bc03c70f64a767091e53995aca1d0ff
Rachel Schwartz <rsschwartz@gmail.com>
2017-05-05 15:02:08 -0400

    update BtG example

--- example.R
+++ example.R
@@ -15,34 +15,6 @@ library(dplyr)
 #from gapminder package the gapminder data
 #install.packages('gapminder')
 gm <- gapminder::gapminder
-
-#misc
-head(gm)
-str(gm)  #structure of data
-class(gm)
-typeof(gm)
-gm$country
-colnames(gm)
-rownames(gm)
-gm_copy<-gm
-colnames(gm_copy) <- c('a','b','c','d','e','f')
-gm[c(3,5),c(2,5)] #subset data - row 3 and 5 for col 2 and 5
-gm[c(3,5),]
-min(gm$lifeExp)
-max(gm$pop)
-min(gm[,5])
-colnames(gm)
-gm$gdp <- gm$pop * gm$gdpPercap
-
-#thinking about vectors
-x <- c(1:4)
-x*2
-x*x
-x>2
-p<-c(.04,.1)
-sig<-p<0.05
-p*x
-
 gm$continent<-as.character(gm$continent)
 gm$pop <- as.numeric(gm$pop)
 gm <-as.data.frame(gm)
@@ -52,8 +24,6 @@ gm <-as.data.frame(gm)
 calc_cont_pop<-function(continent,year){
   sum(gm[gm$year == year & gm$continent == continent,"pop"])
 }
-
-calc_cont_pop('Asia')
 
 cont_list <- unique(as.character(gm$continent))
 pops_all_conts_1997 <- mapply(calc_cont_pop,cont_list,1997)
@@ -65,27 +35,28 @@ pdf("lifeexp_v_time.pdf",width=12,height=4)
 ggplot(data = gm,aes(x=lifeExp,y=gdpPercap,color=country))+
   geom_point()+scale_y_log10()+geom_smooth(method="lm",se = FALSE)+
   theme(legend.position = 'None')
-dev.off()
+
 
 #Split-Apply-Combine with dplyr
-gm_sum <- gm %>% group_by(continent) %>% summarise(gdppercap =mean(gdpPercap))
-ggplot(gm_sum, aes(x=continent,y=gdppercap))+geom_point()
+gm %>% group_by(continent) %>% summarise(gdppercap =mean(gdpPercap)) %>%
+  ggplot(gm_sum, aes(x=continent,y=gdppercap))+geom_point()
 
 gm %>% filter(year == 2007) %>% #filter gm for 1997
   ggplot(aes(x=continent, y=gdpPercap*pop))+geom_point()
 
-filter(gm,year==2007)
+#filter(gm,year==2007)
 
-gm %>% select(country,year,pop) %>% head()
-gm %>% mutate(gdp = gdpPercap*pop) %>% head()
+#gm %>% select(country,year,pop) %>% head()
+#gm %>% mutate(gdp = gdpPercap*pop) %>% head()
 #above same as below
-gm$gdp <- gm$gdpPercap*gm$pop
+#gm$gdp <- gm$gdpPercap*gm$pop
 
 ##MODELS##
 ggplot(gm,aes(x=year,y=lifeExp))+
   xlab("Year") + ylab("Life Expectancy")+
   geom_point() + geom_smooth(method=lm)
+dev.off()
 
 gm_model <- lm(lifeExp ~ year, data = gm)
-summary(gm_model)
+print(summary(gm_model))
 
